68b4be095d52bc7d604f330e617f1736e1d46f23
Fitti <dev@fitti.io>
2026-07-13 14:06:01 +0200

    Clean up big int Zig further
    This is very important for my understanding
    of the C++ programming language. I swear!

--- src/superexp.zig
+++ src/superexp.zig
@@ -6,44 +6,51 @@ pub fn main(init: std.process.Init) !void {
     const arena = init.arena;
 
     const proc_allocator = arena.allocator();
-    var base: BigInt = try .initSet(proc_allocator, 2);
+    const base_int: u8 = 2;
+    var base: BigInt = try .initSet(proc_allocator, base_int);
     var super_exp: BigInt = try .initSet(proc_allocator, 0);
+    var return_value: BigInt = try .init(proc_allocator);
 
     var i: usize = 0;
     while (true) : (i += 1) {
         try super_exp.addScalar(&super_exp, i);
-        const res = superExponential(
+        const res = tetrate(
             proc_allocator,
             &base,
             &super_exp,
+            &return_value,
         ) catch |err| {
             std.debug.print("Believe it or not, something went wrong when calculating the next one: {t}\n", .{err});
             std.debug.print("(Long story short, this error means we tried to fit the above number into 32 bits.)\n", .{});
             break;
         };
-        std.debug.print("{s}_{s} = {s}\n", .{
-            try base.toString(proc_allocator, 10, .lower),
-            try super_exp.toString(proc_allocator, 10, .lower),
+        std.debug.print("{d}↑↑{d} = {s}\n", .{
+            base_int,
+            i,
             try res.toString(proc_allocator, 10, .lower),
         });
     }
 }
 
-fn superExponential(
+fn tetrate(
     gpa: std.mem.Allocator,
     base: *BigInt,
-    super_exp: *BigInt,
-) !BigInt {
-    if (super_exp.eqlZero()) return .initSet(gpa, 0);
-    var value: BigInt = try .init(gpa);
-    try super_exp.addScalar(super_exp, -1);
-    try value.pow(
-        base,
-        try (try superExponential(
-            gpa,
+    height: *BigInt,
+    return_value: *BigInt,
+) !*BigInt {
+    if (height.eqlZero()) {
+        try return_value.set(0);
+    } else {
+        try height.addScalar(height, -1);
+        try return_value.pow(
             base,
-            super_exp,
-        )).toInt(u32),
-    );
-    return value;
+            try (try tetrate(
+                gpa,
+                base,
+                height,
+                return_value,
+            )).toInt(u32),
+        );
+    }
+    return return_value;
 }
