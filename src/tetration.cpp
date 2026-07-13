6e22c0336b625b4f2de9040740402ad4b88cab00
Fitti <dev@fitti.io>
2026-07-13 15:48:53 +0200

    Try out std::expected

--- src/tetration.cpp
+++ src/tetration.cpp
@@ -1,36 +1,42 @@
 #include <climits>
 #include <cmath>
+#include <expected>
 #include <iostream>
 
-int pow(int base, int exp) {
+enum class TetrationError {
+  overflow,
+};
+
+std::expected<int, TetrationError> pow(int base, int exp) {
   int ret = 1;
   for (int i = 0; i < exp; i++) {
     if (INT_MAX / base < ret)
-      return -1;
+      return std::unexpected(TetrationError::overflow);
     ret *= base;
   }
   return ret;
 }
 
-int tetrate(int base, int height) {
+std::expected<int, TetrationError> tetrate(int base, int height) {
   if (height == 0)
     return 1;
-  int exponent = tetrate(base, height - 1);
-  if (exponent < 0)
-    return -1;
-  return pow(base, exponent);
+  auto exponent = tetrate(base, height - 1);
+  if (exponent)
+    return pow(base, exponent.value());
+  return exponent;
 }
 
 int main() {
   for (int i = 0;; i++) {
-    int res = tetrate(2, i);
     std::cout << "2_" << i << " = ";
-    if (res < 0) {
+    auto res = tetrate(2, i);
+    if (res) {
+      std::cout << res.value() << '\n';
+    } else {
       std::cout << "https://sites.google.com/site/largenumbers/home/appendix/a/"
                    "ulnl/265536\n";
       break;
     }
-    std::cout << res << '\n';
   }
 
   return 0;
