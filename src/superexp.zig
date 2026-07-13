365e79daa958469e7db84deaacbc2b93d819a295
Fitti <dev@fitti.io>
2026-07-13 13:31:10 +0200

    Awkwardly stumbling around with Zig's big integers

--- src/superexp.zig
+++ src/superexp.zig
@@ -1,27 +1,58 @@
 const std = @import("std");
 const math = std.math;
+const BigInt = std.math.big.int.Managed;
 
 const int_type = u64;
 
-pub fn main() !void {
-    const base: u8 = 2;
-    var super_exp: u8 = 0;
-    while (true) : (super_exp += 1) {
-        std.debug.print("{d}_{d} = {d}\n", .{
-            base,
-            super_exp,
-            superExponential(base, @truncate(super_exp)) catch break,
+pub fn main(init: std.process.Init) !void {
+    const arena = init.arena;
+
+    const proc_allocator = arena.allocator();
+    const zero: BigInt = try .initSet(proc_allocator, 0);
+    const one: BigInt = try .initSet(proc_allocator, 1);
+    var base: BigInt = try .initSet(proc_allocator, 2);
+    var super_exp: BigInt = try .initSet(proc_allocator, 0);
+
+    while (true) : (try super_exp.add(&super_exp, &one)) {
+        var clone = try super_exp.clone();
+        const res = superExponential(
+            proc_allocator,
+            &zero,
+            &one,
+            &base,
+            &clone,
+        ) catch |err| {
+            std.debug.print("Believe it or not, something went wrong when calculating the next one: {t}\n", .{err});
+            std.debug.print("(Long story short, this error means we tried to fit the above number into 32 bits.)\n", .{});
+            break;
+        };
+        std.debug.print("{s}_{s} = {s}\n", .{
+            try base.toString(proc_allocator, 10, .lower),
+            try super_exp.toString(proc_allocator, 10, .lower),
+            try res.toString(proc_allocator, 10, .lower),
         });
-    } else return;
-    std.debug.print("{d}_{d} = {s}\n", .{
-        base,
-        super_exp,
-        "https://sites.google.com/site/largenumbers/home/appendix/a/ulnl/265536",
-    });
+    }
 }
 
-fn superExponential(base: u8, super_exp: u8) !int_type {
-    if (super_exp == 0) return 0;
-    const big_base: int_type = @intCast(base);
-    return try math.powi(int_type, big_base, try superExponential(base, super_exp - 1));
+fn superExponential(
+    gpa: std.mem.Allocator,
+    zero: *const BigInt,
+    one: *const BigInt,
+    base: *BigInt,
+    super_exp: *BigInt,
+) !BigInt {
+    if (super_exp.eql(zero.*)) return zero.clone();
+    var value: BigInt = try .init(gpa);
+    try super_exp.sub(super_exp, one);
+    try value.pow(
+        base,
+        try (try superExponential(
+            gpa,
+            zero,
+            one,
+            base,
+            super_exp,
+        )).toInt(u32),
+    );
+    return value.clone();
 }
