6af073b6006a0257565c60e4adbb20dab5de88b3
Fitti <dev@fitti.io>
2026-07-13 13:53:53 +0200

    Figure out Zig std.math.big.int.Managed API

--- src/superexp.zig
+++ src/superexp.zig
@@ -2,25 +2,20 @@ const std = @import("std");
 const math = std.math;
 const BigInt = std.math.big.int.Managed;
 
-const int_type = u64;
-
 pub fn main(init: std.process.Init) !void {
     const arena = init.arena;
 
     const proc_allocator = arena.allocator();
-    const zero: BigInt = try .initSet(proc_allocator, 0);
-    const one: BigInt = try .initSet(proc_allocator, 1);
     var base: BigInt = try .initSet(proc_allocator, 2);
     var super_exp: BigInt = try .initSet(proc_allocator, 0);
 
-    while (true) : (try super_exp.add(&super_exp, &one)) {
-        var clone = try super_exp.clone();
+    var i: usize = 0;
+    while (true) : (i += 1) {
+        try super_exp.addScalar(&super_exp, i);
         const res = superExponential(
             proc_allocator,
-            &zero,
-            &one,
             &base,
-            &clone,
+            &super_exp,
         ) catch |err| {
             std.debug.print("Believe it or not, something went wrong when calculating the next one: {t}\n", .{err});
             std.debug.print("(Long story short, this error means we tried to fit the above number into 32 bits.)\n", .{});
@@ -36,23 +31,19 @@ pub fn main(init: std.process.Init) !void {
 
 fn superExponential(
     gpa: std.mem.Allocator,
-    zero: *const BigInt,
-    one: *const BigInt,
     base: *BigInt,
     super_exp: *BigInt,
 ) !BigInt {
-    if (super_exp.eql(zero.*)) return zero.clone();
+    if (super_exp.eqlZero()) return .initSet(gpa, 0);
     var value: BigInt = try .init(gpa);
-    try super_exp.sub(super_exp, one);
+    try super_exp.addScalar(super_exp, -1);
     try value.pow(
         base,
         try (try superExponential(
             gpa,
-            zero,
-            one,
             base,
             super_exp,
         )).toInt(u32),
     );
-    return value.clone();
+    return value;
 }
